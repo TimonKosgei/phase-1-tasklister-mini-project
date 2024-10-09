f30ac163f9d2233ea83a88df1d897a1e40760613
TimonKosgei <timonkosgei15209@gmail.com>
2024-10-09 15:15:17 +0300

    did labs

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  
+  const form = document.querySelector('form');
+  form.addEventListener('submit',(e) =>{
+    const newTask = document.getElementById('new-task-description').value;
+    const list  = document.getElementById('tasks')
+    const li  = document.createElement('li')
+    e.preventDefault();    
+    li.textContent = newTask;
+    list.append(li);
+  })
 });
